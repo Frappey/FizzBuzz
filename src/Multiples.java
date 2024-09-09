b61f1da9687103c81fc08c41387d22f363787472
FraiFish <133361728+FraiFish@users.noreply.github.com>
2024-09-09 18:14:38 -0400

    excluded 0 and 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -4,9 +4,9 @@ public class Multiples {
     }
 
     private static int multiple() {
-        int i = 0;
+        int i = 1;
         int tot = 0;
-        while (i <= 1000) {
+        while (i < 1000) {
             boolean isM3O5 = (i % 3 == 0 || i % 5 == 0);
             if (isM3O5) {
                 tot++;
